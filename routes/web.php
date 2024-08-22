3a7feb6b954ea774ddcdf13dbb4118625f57f95d
Thanapa-c <thanapa.c@kkumail.com>
2024-08-22 17:33:54 +0700

    MyApp Dashbord

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/myshop', [WebController::class, 'myshop'])->name('web.myshop');  //ใช้แทน nane rout 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/myshop',[Webcontroller::class,'myshop'])->name('myshop.blade');
+require __DIR__.'/auth.php';
